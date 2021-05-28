3504c735d64017572eba85c3961af86fd7ddb351
Toby Davis <pencilcaseman@gmail.com>
2021-05-28 23:27:12 +0100

    Bug fixes?

--- librapid/tests/ndarray.cpp
+++ librapid/tests/ndarray.cpp
@@ -8,19 +8,19 @@
 
 int main()
 {
-	auto lhs = ndarray::basic_ndarray<int>(ndarray::extent({2, 2}), 0);
+	auto lhs = ndarray::basic_ndarray<int>(ndarray::extent({2, 3}), 0);
 	auto rhs = ndarray::basic_ndarray<double>(ndarray::extent({2, 2, 1}), 0);
 
 	for (nd_int i = 0; i < ndarray::math::product(lhs.get_extent().get_extent(), lhs.get_extent().ndim()); i++)
 		lhs.set_value(i, i + 1);
-
+	
 	for (nd_int i = 0; i < ndarray::math::product(rhs.get_extent().get_extent(), rhs.get_extent().ndim()); i++)
 		rhs.set_value(i, i + 1);
 
 	std::cout << "LHS:\n" << lhs.str() << "\n\n";
 	std::cout << "LHS:\n" << (-lhs).str() << "\n\n";
 	std::cout << "RHS:\n" << rhs.str() << "\n\n";
-
+	
 	try
 	{
 		std::cout << "Result:\n" << (10. + lhs.transposed()).str() << "\n\n";
