81f8849a42516b5bb738563e3e2a5b04e5171c6c
Toby Davis <pencilcaseman@gmail.com>
2021-05-31 13:26:57 +0100

    Logo and README

---
+++ README.md
@@ -0,0 +1,40 @@
+<p align="center">
+<img src="https://github.com/Pencilcaseman/librapid/blob/master/branding/color.png" width="800"> 
+</p>
+
+[![Build Status](https://github.com/pencilcaseman/librapid/actions/workflows/wheels.yaml/badge.svg)](https://github.com/Pencilcaseman/librapid/actions/workflows/wheels.yaml) [![Documentation Status](https://readthedocs.org/projects/librapid/badge/?version=latest)](https://librapid.readthedocs.io/en/latest/?badge=latest) [![PyPI version fury.io](https://badge.fury.io/py/librapid.svg)](https://pypi.python.org/pypi/librapid/) [![PyPI license](https://img.shields.io/pypi/l/librapid.svg)](https://pypi.python.org/pypi/librapid/) [![PyPI pyversions](https://img.shields.io/pypi/pyversions/librapid.svg)](https://pypi.python.org/pypi/librapid/) [![Downloads per month](https://img.shields.io/pypi/dm/librapid.svg)](https://pypi.python.org/pypi/librapid/)
+
+LibRapid -- a fast, general purpose math and neural network library for C++ and Python
+
+## How it Works
+
+LibRapid is a fully templated header-only C++ library, which can be found at ```./librapid/```. The C++ library is then interfaced with Python using the amazing [PyBind11](https://github.com/pybind/pybind11) library, meaning very little performance is lost between the C++ and Python versions of the library.
+
+LibRapid also aims to provide a consistent interface between the C++ and Python libraries, allowing you to use the library comprehensively in both language.
+
+Additionally, the Python interface has been adjusted slightly from the C++ interface, providing a more "pythonic" feel, while not reducing the functionality in the slightest.
+
+## Installing LibRapid
+
+To install LibRapid as a Python library, simply run ```pip install librapid``` in the command line. Hopefully, there will be precompiled wheels available for your operating system and python version, meaning you will not need a C++ compiler to install it (if this is not the case, a modern C++ compiler will be required)
+
+To use the library for C++ use, a modern C++ compiler will definitely be required. To use the library simply add the directory of this file to your extra include paths, and include the library using ```#include<librapid/librapid.hpp>```
+
+## Documentation
+
+Unfortunately, the documentation cannot currently be built by [ReadTheDocs](https://readthedocs.org/) due to a bug in the version of [Doxygen](https://www.doxygen.nl/index.html) they are using. It would seem that the systems will be getting an upgrade in the near future, hopefully moving to a newer version of Doxygen, though this is still not certain.
+
+If you would like to view the documentation, you will have to build it yourself. This is very simple once you have the correct software installed, most of which can be installed via ```pip```.
+
+```bash
+pip install sphinx
+pip install breathe
+pip install exhale
+pip install furo
+```
+
+You will also need to install a recent version of Doxygen, which you can find [here](https://www.doxygen.nl/download.html)
+
+To build the documentation, open a command line window in the ```docs``` directory and run ```make html```. You can then open the file ```docs/build/html/index.html``` in a web-browser to view the documentation.
+
+Hopefully in the near future, the documentation will be available online, so you will not have to build it yourself...
--- setup.py
+++ setup.py
@@ -4,32 +4,139 @@ from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from pybind11 import get_cmake_dir
 
+from pathlib import Path
+import platform
 import os
 import sys
+
+try:
+	from setuptools import setup, Extension, find_packages
+except ImportError:
+	from distutils.core import setup, Extension
+
+	def find_packages(where='.'):
+		return [folder.replace("/", ".").lstrip(".")
+				for (folder, _, files) in os.walk(where)
+        		if "__init__.py" in files]
+
+def get_compiler_name():
+	import re
+	import distutils.ccompiler
+	comp = distutils.ccompiler.get_default_compiler()
+	getnext = False
+
+	for a in sys.argv[2:]:
+		if getnext:
+			comp = a
+			getnext = False
+			continue
+		# Separated by space
+		if a == '--compiler'  or  re.search('^-[a-z]*c$', a):
+			getnext = True
+			continue
+		# Without space
+		m = re.search('^--compiler=(.+)', a)
+		if m == None:
+			m = re.search('^-[a-z]*c(.+)', a)
+		if m:
+			comp = m.group(1)
+
+	return comp
 
 # Load the version number from VERSION.hpp
 version_file = open("librapid/VERSION.hpp", "r")
 __version__ = version_file.readlines()[1].split()[2].replace("\"", "")
 version_file.close()
 
+# Locate and read the contents of README.md
+long_description = Path("./README.md").read_text(encoding="utf-8")
+
+# Set the C++ version to use
+def std_version():
+	c = get_compiler_name()
+	if c == "msvc": return ["/std:c++latest"]
+	elif c in ("gcc", "g++"): return ["-std=c17"]
+	elif c == "clang": return ["-std=c17"]
+	elif c == "unix": return ["-std=c17"]
+	return []
+
+def compile_with_omp():
+	if platform.system() == "Darwin":
+		return []
+
+	c = get_compiler_name()
+	if c == "msvc": return ["/openmp"]
+	elif c in ("gcc", "g++"): return ["-fopenmp"]
+	elif c == "clang": return ["-fopenmp"]
+	elif c == "unix": return ["-fopenmp"]
+	return []
+
+def link_omp():
+	if platform.system() == "Darwin":
+		return []
+	
+	c = get_compiler_name()
+	if c == "msvc": return []
+	elif c in ("gcc", "g++"): return ["-lgomp"]
+	elif c == "unix": return ["-lgomp"]
+	elif c == "clang": return ["-lgomp"]
+	return []
+
+def enable_optimizations():	
+	c = get_compiler_name()
+	if c == "msvc":
+		res = ["/O2", "/Ot", "/Ob1"]
+		p = platform.processor().split()[0]
+		if p == "AMD64":
+			res += ["/favor:AMD64"]
+		elif p == "INTEL64":
+			res += ["/favor:INTEL64"]
+		elif p == "ATOM":
+			res += ["/favor:ATOM"]
+		return res
+	elif c in ("gcc", "g++"): return ["-O3", "-mavx"]
+	elif c == "clang": return ["-O3", "-mavx"]
+	elif c == "unix": return ["-O3", "-mavx"]
+	return []
+
+compiler_flags = std_version() + compile_with_omp() + enable_optimizations()
+linker_flags = link_omp()
+
 ext_modules = [
-    Pybind11Extension("librapid",
-        ["librapid/pybind_librapid.cpp"],
-        define_macros = [('LIBRAPID_BUILD', 1)],
-        include_dirs=[os.getcwd()]
-        )
+	Pybind11Extension("librapid",
+		["librapid/pybind_librapid.cpp"],
+		extra_compile_args=compiler_flags,
+		extra_link_args=linker_flags,
+		define_macros = [('LIBRAPID_BUILD', 1)],
+		include_dirs=[os.getcwd()]
+		)
 ]
 
 setup(
-    name="librapid",
-    version=__version__,
-    author="Toby Davis",
-    author_email="pencilcaseman@gmail.com",
-    url="https://github.com/Pencilcaseman/librapid",
-    description="A fast math and neural network library for Python and C++",
-    long_description="",
-    ext_modules=ext_modules,
-    extras_require={"test": "pytest"},
-    cmdclass={"build_ext": build_ext},
-    zip_safe=False
+	name="librapid",
+	version=__version__,
+	author="Toby Davis",
+	author_email="pencilcaseman@gmail.com",
+	url="https://github.com/Pencilcaseman/librapid",
+	description="A fast math and neural network library for Python and C++",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	ext_modules=ext_modules,
+	license="Boost Software License",
+	keywords=["math", "neural network", "ndarray", "array", "matrix",
+			"high-performance computing"],
+	classifiers=[
+		"Development Status :: 2 - Pre-Alpha",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Education",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9",
+	],
+	extras_require={"test": "pytest"},
+	cmdclass={"build_ext": build_ext},
+	zip_safe=False
 )
