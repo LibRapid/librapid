a755c30e6b4be2541cb845521f5b4edf187491a1
Toby Davis <pencilcaseman@gmail.com>
2021-08-20 11:29:02 +0100

    Please do something better this time...
    Former-commit-id: 6eb42abc1ea6797e31994be21396c9d39562a086 [formerly e1079c2ceb83b0b245ce8d10d18fafd80a3defbd]
    Former-commit-id: 34b3d986a3e8e776192c7510be4ca3216d89b800

--- src/librapid/__init__.py
+++ src/librapid/__init__.py
@@ -7,28 +7,16 @@ from pathlib import Path
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-sys.path.insert(1, ROOT_DIR)
-sys.path.insert(1, os.path.join(ROOT_DIR, os.pardir))
-
-# If using windows, tell the os where the DLL for blas is
-# If blas was not installed, this doesn't really do anything
-if platform.system() == "Windows":
-	import win32api
-
-	print("Loading DLL from", os.path.join(ROOT_DIR, "blas"))
-	win32api.SetDllDirectory(os.path.join(ROOT_DIR, "blas"))
-	sys.path.append(os.path.join(ROOT_DIR, "blas"))
-
 try:
 	try:
-		print("Attempting to load '.librapidcore' from {}".format(os.listdir(ROOT_DIR)))
+		print("Attempting to load '._librapid' from {}".format(os.listdir(ROOT_DIR)))
 		from ._librapid import *
 	except ModuleNotFoundError:
-		print("Failed to load '.librapid'. Attempting to load 'librapidcore' globally")
+		print("Failed to load '._librapid'. Attempting to load '_librapid' globally")
 		from _librapid import *
 except ImportError:
-	print("There was an error trying to load the librapid C++ module '_librapid'.")
+	print("There was an error trying to load the librapid C++ module '_librapid'")
 	
 	if platform.system() == "Windows":
-		print("This could be caused by a missing DLL file in the 'librapid/blas' directory")
+		print("This could be caused by a missing BLAS DLL file in the '{}' directory".format(ROOT_DIR))
 		print("\n".join(os.listdir(ROOT_DIR, "blas")))
