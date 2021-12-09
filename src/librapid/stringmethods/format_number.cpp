7e5cdd0ed864b41d3e088c75dd3bf3f0584cc435
Maximilian <57870403+NervousNullPtr@users.noreply.github.com>
2021-12-09 17:18:04 +0100

    New functionality in number formatting

--- src/librapid/stringmethods/format_number.cpp
+++ src/librapid/stringmethods/format_number.cpp
@@ -2,18 +2,29 @@
 #include <librapid/autocast/custom_complex.hpp>
 #include <string>
 #include <sstream>
+#include <algorithm>
 
 namespace librapid {
-	std::string format_number(const double &val, bool floating) {
+	std::string format_number(const double &val, bool floating, bool international) {
 		std::stringstream stream;
 		stream.precision(10);
 
 		stream << val;
 
 		std::string str = stream.str();
-		if (floating && str.find_last_of('.') == std::string::npos)
-			str += ".";
 
-		return str;
+		switch (international) {
+			case true:
+				if (floating && str.find_last_of('.') == std::string::npos)
+					str += ".0";
+
+				return str;
+			case false:
+				if (floating && str.find_last_of('.') == std::string::npos)
+					str += ",0";
+				else
+					std::replace(str.begin(), str.end(), '.', ',');
+				return str;
+		}
 	}
 }
