773c003456a6d69d5111324846c4aafb548682e2
Pencilcaseman <pencilcaseman@gmail.com>
2021-12-22 23:22:29 -0800

    Bug fixes
    Former-commit-id: d25159a57fb2e2ca819f19ef4ebc9ada1ae319e5 [formerly 1c1845b829b6bf4e8a2f6517280c43bc344d30ef]
    Former-commit-id: 413a271fe544739b0c628a78b2c47e4c8501200d

--- src/librapid/stringmethods/format_number.hpp
+++ src/librapid/stringmethods/format_number.hpp
@@ -2,13 +2,20 @@
 #define LIBRAPID_FORMAT_NUMBER
 
 #include <librapid/config.hpp>
+#include <string>
+#include <sstream>
 
 namespace librapid {
-	template<typename T>
-	class Complex;
-
-	std::string format_number(const double &val, bool floating = true, bool international = true);
-	std::string format_number(const Complex<double> &val, bool floating = true, bool international = true);
+    template<typename T>
+    std::string format_number(const T &val, bool floating = std::is_floating_point<T>::value) {
+        std::stringstream stream;
+        stream.precision(10);
+        stream << val;
+        std::string str = stream.str();
+        if (floating && str.find_last_of('.') == std::string::npos)
+            str += ".";
+        return str;
+    }
 }
 
 #endif // LIBRAPID_FORMAT_NUMBER
