973eb4bf9075eaa90a96c7cc37c39f1ba9943f4c
Toby <pencilcaseman@gmail.com>
2022-04-11 21:52:04 +0100

    Did some stuff
    Former-commit-id: 5b850e209922be8e80ca4b50fa521b68ad7edbce

--- src/librapid/librapid.hpp
+++ src/librapid/librapid.hpp
@@ -4,11 +4,13 @@
 #include "VERSION.hpp"
 #include "internal/config.hpp"
 #include "internal/memUtils.hpp"
+#include "utils/time.hpp"
 #include "internal/forward.hpp"
-#include "array/packet.hpp"
+#include "array/traits.hpp"
 #include "array/denseStorage.hpp"
 #include "array/helpers/extent.hpp"
 #include "array/arrayBase.hpp"
+#include "array/cwisebinop.hpp"
 #include "array/array.hpp"
 
 #endif // LIBRAPID_INCLUDE
