2382c3eb2d060bdd1b8ef30411e53ce02bf4aa0e
Pencilcaseman <pencilcaseman@gmail.com>
2022-06-29 22:52:52 +0100

    maybe?
    Former-commit-id: c918c40efa48f46874ef0599e1f6c58ef29c08de

--- tests/python/testArrayPrinting.py
+++ tests/python/testArrayPrinting.py
@@ -1,10 +1,14 @@
 import librapid as lrp
 
-# Simple array formatting. This tests indexing as well,
-# and if this succeeds, it allows us to test other features
-# more easily as we know certain fundamental operations are
-# working as expected
-x = lrp.ArrayF32(lrp.Extent([5]))
-for i in range(5):
-    x[i] = i + 1
-assert(x.str() == "[1 2 3 4 5]")
+def testArrayPrint():
+    """
+    Simple array formatting. This tests indexing as well,
+    and if this succeeds, it allows us to test other features
+    more easily as we know certain fundamental operations are
+    working as expected
+    """
+    
+    x = lrp.ArrayF32(lrp.Extent([5]))
+    for i in range(5):
+        x[i] = i + 1
+    assert(x.str() == "[1 2 3 4 5]")
