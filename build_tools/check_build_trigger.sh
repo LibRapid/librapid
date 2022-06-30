78d3275117c105cc77550b187ba66876135c035a
Pencilcaseman <pencilcaseman@gmail.com>
2022-06-30 09:08:51 +0100

    yeah...
    Former-commit-id: a1edeb1228ef908ced5cefaf7322f76ab7d66844 [formerly d5cbb95ed01663b86d7bee18224ddf415750987d]
    Former-commit-id: e47a17b129eec31a88429455cf53191008d54a90

--- build_tools/check_build_trigger.sh
+++ build_tools/check_build_trigger.sh
@@ -5,9 +5,11 @@ set -x
 
 COMMIT_MSG=$(git log --no-merges -1 --oneline)
 
-# The commit marker "[ci build]" or "[ci build gh]" will trigger the build when required
-if [[ "$GITHUB_EVENT_NAME" == schedule ||
-      "$COMMIT_MSG" =~ \[ci\ build\] ||
-      "$COMMIT_MSG" =~ \[ci\ build\ gh\] ]]; then
-    echo "::set-output name=build::true"
-fi
+# if [[ "$GITHUB_EVENT_NAME" == schedule ||
+#       "$COMMIT_MSG" =~ \[ci\ build\] ||
+#       "$COMMIT_MSG" =~ \[ci\ build\ gh\] ]]; then
+#     echo "::set-output name=build::true"
+# fi
+
+# I don't know how to do this, so I'm just gonna cheat
+echo "::set-output name=build::true"
