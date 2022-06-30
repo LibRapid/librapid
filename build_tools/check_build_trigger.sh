8ac1a329f38384ee08783362b228916fa7aaadf5
Pencilcaseman <pencilcaseman@gmail.com>
2022-06-30 09:06:36 +0100

    ci build
    Former-commit-id: 23ab64b2cb2a7ed6cdb4715574010ebf5054b1ad

--- build_tools/check_build_trigger.sh
+++ build_tools/check_build_trigger.sh
@@ -5,9 +5,9 @@ set -x
 
 COMMIT_MSG=$(git log --no-merges -1 --oneline)
 
-# The commit marker "[cd build]" or "[cd build gh]" will trigger the build when required
+# The commit marker "[ci build]" or "[ci build gh]" will trigger the build when required
 if [[ "$GITHUB_EVENT_NAME" == schedule ||
-      "$COMMIT_MSG" =~ \[cd\ build\] ||
-      "$COMMIT_MSG" =~ \[cd\ build\ gh\] ]]; then
+      "$COMMIT_MSG" =~ \[ci\ build\] ||
+      "$COMMIT_MSG" =~ \[ci\ build\ gh\] ]]; then
     echo "::set-output name=build::true"
 fi
