13c7271f61b2ab006974cc417536dd0caf8a6adf
Pencilcaseman <pencilcaseman@gmail.com>
2022-06-30 14:17:15 +0100

    Added initial python wrapper
    Former-commit-id: 9ca0f3bba40103a8efd5b005225aa93188a72b74 [formerly 2b79971b6e8eb3baa9800aa119ffc645316ee63b]
    Former-commit-id: 8740cc0afb069935d079545ba402e12e13ca3e44

---
+++ src/librapid/python/typeMapping.py
@@ -0,0 +1,31 @@
+# NOTE: Here, None represents a default, unintialized value
+
+typeMap = {
+	"ArrayB" : ("b", "bool", "boolean"),
+	"ArrayC" : ("c", "char", "byte", "character"),
+	"ArrayF16" : ("half", "float16", "f16"),
+	"ArrayF32" : ("float", "float32", "f32", "default", None),
+	"ArrayF64" : ("double", "float64", "f64"),
+	"ArrayI16" : ("short", "int16", "i16"),
+	"ArrayI32" : ("int", "long", "int32", "i32"),
+	"ArrayI64" : ("long long", "int64", "i64")
+}
+
+deviceMap = {
+	"CPU" : ("cpu", "host", "default", None),
+	"GPU" : ("gpu", "cuda", "device")
+}
+
+def mapType(type:str = None, device:str = None):
+	resType = None
+	resDevice = None
+	
+	for key, val in typeMap.items():
+		if type in val:
+			resType = key
+
+	for key, val in deviceMap.items():
+		if device in val:
+			resDevice = key
+
+	return resType, resDevice
