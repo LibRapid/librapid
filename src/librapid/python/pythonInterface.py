ab9176a12385fbe1f1cb54f471467135ca11ccb3
Pencilcaseman <pencilcaseman@gmail.com>
2022-07-04 19:50:54 +0100

    yep forgot to add the actual changes...
    Former-commit-id: 51116dccd814152938df79e480e5808fd7ded6da

--- src/librapid/python/pythonInterface.py
+++ src/librapid/python/pythonInterface.py
@@ -1,17 +1,17 @@
-from argparse import ArgumentError
 import _librapid
 from . import typeMapping
+from . import extentInterface
 
 print("HELLO WORLD!")
 
 def test(x):
     print(x * 2)
 
-Extent = _librapid.Extent
+Extent = extentInterface.Extent
 
 class Array:
 	def __init__(self, *args, **kwargs):
-		self.array = None
+		self._array = None
 		type = None
 		device = None
 		extent = None
@@ -44,9 +44,9 @@ class Array:
 									_librapid.ArrayI16,
 									_librapid.ArrayI32,
 									_librapid.ArrayI64)):
-				self.array = args[0]
+				self._array = args[0]
 
-			elif isinstance(args[0], _librapid.Extent):
+			elif isinstance(args[0], Extent):
 				extent = args[0]
 
 				if len(args) > 1:
@@ -54,175 +54,175 @@ class Array:
 				if len(args) > 2:
 					device = args[2]
 				if len(args) > 3:
-					raise ArgumentError("Array constructor expects <Extent, [type], [device]>")
+					raise ValueError("Array constructor expects <Extent, [type], [device]>")
 			else:
-				raise ArgumentError("Extent of Array is required")
+				raise ValueError("Extent of Array is required")
 
 		adjustedType, adjustedDevice = typeMapping.mapType(type, device)
 
 		if adjustedType is None or adjustedDevice is None:
 			raise ValueError("Array with type={} and device={} is invalid".format(type, device))
 
-		if self.array is None:
+		if self._array is None:
 			if adjustedDevice == "CPU":
 				if adjustedType == "ArrayB":
-					self.array = _librapid.ArrayB(extent)
+					self._array = _librapid.ArrayB(extent)
 				elif adjustedType == "ArrayC":
-					self.array = _librapid.ArrayC(extent)
+					self._array = _librapid.ArrayC(extent._extent)
 				elif adjustedType == "ArrayF16":
-					self.array = _librapid.ArrayF16(extent)
+					self._array = _librapid.ArrayF16(extent._extent)
 				elif adjustedType == "ArrayF32":
-					self.array = _librapid.ArrayF32(extent)
+					self._array = _librapid.ArrayF32(extent._extent)
 				elif adjustedType == "ArrayF64":
-					self.array = _librapid.ArrayF64(extent)
+					self._array = _librapid.ArrayF64(extent._extent)
 				elif adjustedType == "ArrayI16":
-					self.array = _librapid.ArrayI16(extent)
+					self._array = _librapid.ArrayI16(extent._extent)
 				elif adjustedType == "ArrayI32":
-					self.array = _librapid.ArrayI32(extent)
+					self._array = _librapid.ArrayI32(extent._extent)
 				elif adjustedType == "ArrayI64":
-					self.array = _librapid.ArrayI64(extent)
+					self._array = _librapid.ArrayI64(extent._extent)
 			elif adjustedDevice == "GPU":
 				if adjustedType == "ArrayB":
-					self.array = _librapid.ArrayBG(extent)
+					self._array = _librapid.ArrayBG(extent._extent)
 				elif adjustedType == "ArrayC":
-					self.array = _librapid.ArrayCG(extent)
+					self._array = _librapid.ArrayCG(extent._extent)
 				elif adjustedType == "ArrayF16":
-					self.array = _librapid.ArrayF16G(extent)
+					self._array = _librapid.ArrayF16G(extent._extent)
 				elif adjustedType == "ArrayF32":
-					self.array = _librapid.ArrayF32G(extent)
+					self._array = _librapid.ArrayF32G(extent._extent)
 				elif adjustedType == "ArrayF64":
-					self.array = _librapid.ArrayF64G(extent)
+					self._array = _librapid.ArrayF64G(extent._extent)
 				elif adjustedType == "ArrayI16":
-					self.array = _librapid.ArrayI16G(extent)
+					self._array = _librapid.ArrayI16G(extent._extent)
 				elif adjustedType == "ArrayI32":
-					self.array = _librapid.ArrayI32G(extent)
+					self._array = _librapid.ArrayI32G(extent._extent)
 				elif adjustedType == "ArrayI64":
-					self.array = _librapid.ArrayI64G(extent)
+					self._array = _librapid.ArrayI64G(extent._extent)
 
 	def copy(self):
-		return Array(self.array.copy())
+		return Array(self._array.copy())
 
 	def __getitem__(self, index:int):
-		return Array(self.array[index])
+		return Array(self._array[index])
 	
 	def __setitem__(self, index:int, val):
-		self.array[index] = val
+		self._array[index] = val
 
 	def scalar(self, index:int):
-		return self.array.scalar(index)
+		return self._array.scalar(index)
 
 	def move(self, newLoc:str):
 		_, adjusted = typeMapping.mapType(None, newLoc)
 		if adjusted == "CPU":
-			return Array(self.array.move_CPU())
+			return Array(self._array.move_CPU())
 		elif adjusted == "GPU":
-			return Array(self.array.move_GPU())
+			return Array(self._array.move_GPU())
 		
 		raise ValueError("Location \"{}\" is invalid".format(newLoc))
 
 	def cast(self, newType:str):
 		adjustedType, _ = typeMapping.mapType(newType, None)
 		if adjustedType == "ArrayB":
-			return Array(self.array.cast_ArrayB())
+			return Array(self._array.cast_ArrayB())
 		elif adjustedType == "ArrayC":
-			return Array(self.array.cast_ArrayC())
+			return Array(self._array.cast_ArrayC())
 		elif adjustedType == "ArrayF16":
-			return Array(self.array.cast_Array16F())
+			return Array(self._array.cast_Array16F())
 		elif adjustedType == "ArrayF32":
-			return Array(self.array.cast_ArrayF32())
+			return Array(self._array.cast_ArrayF32())
 		elif adjustedType == "ArrayF64":
-			return Array(self.array.cast_ArrayF64())
+			return Array(self._array.cast_ArrayF64())
 		elif adjustedType == "ArrayI16":
-			return Array(self.array.cast_ArrayI16())
+			return Array(self._array.cast_ArrayI16())
 		elif adjustedType == "ArrayI32":
-			return Array(self.array.cast_ArrayI32())
+			return Array(self._array.cast_ArrayI32())
 		elif adjustedType == "ArrayI64":
-			return Array(self.array.cast_ArrayI64())
+			return Array(self._array.cast_ArrayI64())
 
 	def castMove(self, newType:str, newLoc:str):
 		adjustedType, adjustedDevice = typeMapping.mapType(newType, newLoc)
 
 		if adjustedDevice == "CPU":
 			if adjustedType == "ArrayB":
-				return Array(self.array.cast_ArrayB_CPU())
+				return Array(self._array.cast_ArrayB_CPU())
 			elif adjustedType == "ArrayC":
-				return Array(self.array.cast_ArrayC_CPU())
+				return Array(self._array.cast_ArrayC_CPU())
 			elif adjustedType == "ArrayF16":
-				return Array(self.array.cast_Array16F_CPU())
+				return Array(self._array.cast_Array16F_CPU())
 			elif adjustedType == "ArrayF32":
-				return Array(self.array.cast_ArrayF32_CPU())
+				return Array(self._array.cast_ArrayF32_CPU())
 			elif adjustedType == "ArrayF64":
-				return Array(self.array.cast_ArrayF64_CPU())
+				return Array(self._array.cast_ArrayF64_CPU())
 			elif adjustedType == "ArrayI16":
-				return Array(self.array.cast_ArrayI16_CPU())
+				return Array(self._array.cast_ArrayI16_CPU())
 			elif adjustedType == "ArrayI32":
-				return Array(self.array.cast_ArrayI32_CPU())
+				return Array(self._array.cast_ArrayI32_CPU())
 			elif adjustedType == "ArrayI64":
-				return Array(self.array.cast_ArrayI64_CPU())
+				return Array(self._array.cast_ArrayI64_CPU())
 		elif adjustedDevice == "GPU":
 			if adjustedType == "ArrayB":
-				return Array(self.array.cast_ArrayB_GPU())
+				return Array(self._array.cast_ArrayB_GPU())
 			elif adjustedType == "ArrayC":
-				return Array(self.array.cast_ArrayC_GPU())
+				return Array(self._array.cast_ArrayC_GPU())
 			elif adjustedType == "ArrayF16":
-				return Array(self.array.cast_Array16F_GPU())
+				return Array(self._array.cast_Array16F_GPU())
 			elif adjustedType == "ArrayF32":
-				return Array(self.array.cast_ArrayF32_GPU())
+				return Array(self._array.cast_ArrayF32_GPU())
 			elif adjustedType == "ArrayF64":
-				return Array(self.array.cast_ArrayF64_GPU())
+				return Array(self._array.cast_ArrayF64_GPU())
 			elif adjustedType == "ArrayI16":
-				return Array(self.array.cast_ArrayI16_GPU())
+				return Array(self._array.cast_ArrayI16_GPU())
 			elif adjustedType == "ArrayI32":
-				return Array(self.array.cast_ArrayI32_GPU())
+				return Array(self._array.cast_ArrayI32_GPU())
 			elif adjustedType == "ArrayI64":
-				return Array(self.array.cast_ArrayI64_GPU())
+				return Array(self._array.cast_ArrayI64_GPU())
 
 	def __add__(self, other):
 		if isinstance(other, Array):
-			return Array(self.array + other.array)
-		return Array(self.array + other)
+			return Array(self._array + other.array)
+		return Array(self._array + other)
 
 	def __sub__(self, other):
 		if isinstance(other, Array):
-			return Array(self.array - other.array)
-		return Array(self.array - other)
+			return Array(self._array - other.array)
+		return Array(self._array - other)
 
 	def __mul__(self, other):
 		if isinstance(other, Array):
-			return Array(self.array * other.array)
-		return Array(self.array * other)
+			return Array(self._array * other.array)
+		return Array(self._array * other)
 
 	def __div__(self, other):
 		if isinstance(other, Array):
-			return Array(self.array / other.array)
-		return Array(self.array / other)
+			return Array(self._array / other.array)
+		return Array(self._array / other)
 
 	def __or__(self, other):
 		if isinstance(other, Array):
-			return Array(self.array | other.array)
-		return Array(self.array | other)
+			return Array(self._array | other.array)
+		return Array(self._array | other)
 
 	def __and__(self, other):
 		if isinstance(other, Array):
-			return Array(self.array & other.array)
-		return Array(self.array & other)
+			return Array(self._array & other.array)
+		return Array(self._array & other)
 
 	def __xor__(self, other):
 		if isinstance(other, Array):
-			return Array(self.array ^ other.array)
-		return Array(self.array ^ other)
+			return Array(self._array ^ other.array)
+		return Array(self._array ^ other)
 
 	def __neg__(self):
-		return Array(-self.array)
+		return Array(-self._array)
 
 	def transpose(self, order:Extent=[]):
-		self.array.transpose(order)
+		self._array.transpose(order)
 
 	def transposed(self, order:Extent=[]):
-		return Array(self.array.transposed(order))
+		return Array(self._array.transposed(order))
 
 	def dot(self, other):
-		return Array(self.array.dot(other.array))
+		return Array(self._array.dot(other.array))
 
 	def str(self, format:str="{}",
 				  delim:str=" ",
@@ -230,21 +230,24 @@ class Array:
 				  beforePoint:int=-1,
 				  afterPoint:int=-1,
 				  depth:int=0):
-		return self.array.str(format,
+		return self._array.str(format,
 							  delim,
 							  stripWidth,
 							  beforePoint,
 							  afterPoint,
 							  depth)
 
+	def __str__(self):
+		return self.str()
+
 	def __repr__(self):
-		return repr(self.array)
+		return repr(self._array)
 
 	def isScalar(self):
-		return self.array.isScalar()
+		return self._array.isScalar()
 
 	def extent(self):
-		return self.array.extent()
+		return self._array.extent()
 
 def add(lhs:Array, rhs:Array, dst:Array):
 	_librapid.add(lhs.array, rhs.array, dst.array)
