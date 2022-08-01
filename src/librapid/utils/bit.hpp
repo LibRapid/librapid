8ae2ab83ccb071fd4b7fd4a8b56d025819d90729
Toby Davis <pencilcaseman@gmail.com>
2022-08-02 00:06:09 +0100

    Fix bit-cast on non-MSVC compilers

--- src/librapid/utils/bit.hpp
+++ src/librapid/utils/bit.hpp
@@ -6,6 +6,10 @@ namespace librapid {
 	template<typename To, typename From>
 	LR_NODISCARD("")
 	LR_INLINE constexpr To bitCast(const From &val) noexcept {
+		static_assert(
+		  sizeof(To) == sizeof(From),
+		  "Types have different sizes, and cannot be cast bit-for-bit between each other");
+
 #if defined(__CUDACC__)
 		To toOjb; // assumes default-init
 		::std::memcpy(::std::memcpy::addressof(toOjb), ::std::memcpy::addressof(val), sizeof(To));
@@ -14,7 +18,7 @@ namespace librapid {
 		// MSVC doesn't support std::bit_cast until C++20
 		return __builtin_bit_cast(To, val);
 #else
-		return std::bit_cast<To>(val);
+		return *(To *)(&val);
 #endif
 	}
 } // namespace librapid
