37da5fa778a48a66f8061131c6b437a78ddf7d5a
Toby Davis <pencilcaseman@gmail.com>
2022-08-01 14:17:34 +0100

    Hopefully fix all the problems
    Former-commit-id: 5b39516158fac5b69197ab79361e12e46b495340

--- src/librapid/utils/bit.hpp
+++ src/librapid/utils/bit.hpp
@@ -10,8 +10,11 @@ namespace librapid {
 		To toOjb; // assumes default-init
 		::std::memcpy(::std::memcpy::addressof(toOjb), ::std::memcpy::addressof(val), sizeof(To));
 		return _To_obj;
-#else
+#elif defined(LIBRAPID_MSVC_CXX)
+		// MSVC doesn't support std::bit_cast until C++20
 		return __builtin_bit_cast(To, val);
+#else
+		return std::bit_cast<To>(val);
 #endif
 	}
 } // namespace librapid
