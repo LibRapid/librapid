0f7c5f78aa543840c6f211f4c6e9af14082e76d0
Toby Davis <pencilcaseman@gmail.com>
2022-08-14 00:46:26 +0100

    Initial gemm support
    Former-commit-id: 4100649823bca65d1a957befa12dc3484c08955c [formerly b460000615b836619e91967e957e8cfa4f7b8b6f]
    Former-commit-id: 6c081d3dda97129fdbfe696a98dc2a395c8d5478

---
+++ src/librapid/src/linalg/gemm.cpp
@@ -0,0 +1,61 @@
+#include <librapid/internal/config.hpp>
+#include <librapid/linalg/blasInterface.hpp>
+#include <librapid/internal/memUtils.hpp>
+#include <librapid/modified/modified.hpp>
+
+namespace librapid::blas {
+#if defined(LIBRAPID_HAS_BLAS)
+	template<>
+	void gemm<device::CPU, float, float, float>(bool transA, bool transB, int64_t m, int64_t n,
+												int64_t k, float alpha, const float *__restrict a,
+												int64_t lda, const float *__restrict b, int64_t ldb,
+												float beta, float *__restrict c, int64_t ldc) {
+		CBLAS_TRANSPOSE tmpTransA = CblasNoTrans, tmpTransB = CblasNoTrans;
+		if (transA) tmpTransA = CblasTrans;
+		if (transB) tmpTransB = CblasTrans;
+		cblas_sgemm(CblasRowMajor,
+					tmpTransA,
+					tmpTransB,
+					(blasint)m,
+					(blasint)n,
+					(blasint)k,
+					alpha,
+					a,
+					(blasint)lda,
+					b,
+					(blasint)ldb,
+					beta,
+					c,
+					(blasint)ldc);
+	}
+
+	template<>
+	void gemm<device::CPU, double, double, double>(bool transA, bool transB, int64_t m, int64_t n,
+												   int64_t k, double alpha,
+												   const double *__restrict a, int64_t lda,
+												   const double *__restrict b, int64_t ldb,
+												   double beta, double *__restrict c, int64_t ldc) {
+		CBLAS_TRANSPOSE tmpTransA = CblasNoTrans, tmpTransB = CblasNoTrans;
+		if (transA) tmpTransA = CblasTrans;
+		if (transB) tmpTransB = CblasTrans;
+		cblas_dgemm(CblasRowMajor,
+					tmpTransA,
+					tmpTransB,
+					(blasint)m,
+					(blasint)n,
+					(blasint)k,
+					alpha,
+					a,
+					(blasint)lda,
+					b,
+					(blasint)ldb,
+					beta,
+					c,
+					(blasint)ldc);
+	}
+#endif
+
+#if defined(LIBRAPID_HAS_CUDA)
+
+#endif
+} // namespace librapid::blas
