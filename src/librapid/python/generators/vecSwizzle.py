5325c77f520a3fad4222ff2dfd161e2afe7ff54f
Pencilcaseman <pencilcaseman@gmail.com>
2022-09-22 17:40:32 +0100

    Update vector library with more features
    Former-commit-id: e7088f96c51d476bf8938a79be6ebbbf43389d33 [formerly 99b5d7656057aaa5b51eb9b2ff426c0e243b5739]
    Former-commit-id: 49db4c5937f582720b4a7b7aca3b4c281bf0ef55

--- src/librapid/python/generators/vecSwizzle.py
+++ src/librapid/python/generators/vecSwizzle.py
@@ -4,5 +4,5 @@ toSwizzle = ["xy", "xyz", "xyzw"]
 
 for swiz in toSwizzle:
     for perm in itertools.permutations(list(swiz)):
-        term = ", ".join(perm)
-        print(f"""inline Vec<DTYPE, {len(perm)}> {"".join(perm)}() const {{ return {{ {term} }}; }}""")
+        term = ", ".join([x + "()" for x in perm])
+        print(f"""inline VecImpl<Scalar, {len(perm)}, StorageType> {"".join(perm)}() const {{ return {{ {term} }}; }}""")
