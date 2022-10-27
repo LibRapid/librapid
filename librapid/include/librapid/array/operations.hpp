750060b42b9d59ab6d5dc843430557bfa8377d59
Pencilcaseman <pencilcaseman@gmail.com>
2022-10-27 12:37:13 -0700

    Testing and benchmarks
    Former-commit-id: b64c96f18f8b32f416d5ea5053ad3d21390a6a90

--- librapid/include/librapid/array/operations.hpp
+++ librapid/include/librapid/array/operations.hpp
@@ -1,7 +1,7 @@
 #ifndef LIBRAPID_ARRAY_OPERATIONS_HPP
 #define LIBRAPID_ARRAY_OPERATIONS_HPP
 
-#define LIBRAPID_BINARY_OPERATION(NAME_, OP_)                                                      \
+#define LIBRAPID_BINARY_FUNCTOR(NAME_, OP_)                                                        \
 	struct NAME_ {                                                                                 \
 		template<typename T, typename V>                                                           \
 		LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE auto operator()(const T &lhs,                    \
@@ -16,6 +16,15 @@
 		}                                                                                          \
 	}
 
+#define LIBRAPID_BINARY_OPERATION(NAME_, OP_)                                                      \
+	template<class LHS, class RHS>                                                                 \
+	LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE auto operator OP_(LHS &&lhs, RHS &&rhs)              \
+	  LIBRAPID_RELEASE_NOEXCEPT->detail::Function<detail::NAME_, LHS, RHS> {                       \
+		LIBRAPID_ASSERT(lhs.shape() == rhs.shape(), "Shapes must be equal");                       \
+		return detail::makeFunction<detail::NAME_>(std::forward<LHS>(lhs),                         \
+												   std::forward<RHS>(rhs));                        \
+	}
+
 namespace librapid {
 	namespace detail {
 		template<typename Functor, typename... Args>
@@ -24,18 +33,17 @@ namespace librapid {
 			return OperationType(Functor(), std::forward<Args>(args)...);
 		}
 
-		LIBRAPID_BINARY_OPERATION(Plus, +);
-		LIBRAPID_BINARY_OPERATION(Minus, -);
-		LIBRAPID_BINARY_OPERATION(Multiply, *);
-		LIBRAPID_BINARY_OPERATION(Divide, /);
+		LIBRAPID_BINARY_FUNCTOR(Plus, +);	  // a + b
+		LIBRAPID_BINARY_FUNCTOR(Minus, -);	  // a - b
+		LIBRAPID_BINARY_FUNCTOR(Multiply, *); // a * b
+		LIBRAPID_BINARY_FUNCTOR(Divide, /);	  // a / b
+
 	} // namespace detail
 
-	template<class LHS, class RHS>
-	LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE auto operator+(LHS &&lhs, RHS &&rhs)
-	  LIBRAPID_RELEASE_NOEXCEPT->detail::Function<detail::Plus, LHS, RHS> {
-		LIBRAPID_ASSERT(lhs.shape() == rhs.shape(), "Shapes must be equal");
-		return detail::makeFunction<detail::Plus>(std::forward<LHS>(lhs), std::forward<RHS>(rhs));
-	}
+	LIBRAPID_BINARY_OPERATION(Plus, +)
+	LIBRAPID_BINARY_OPERATION(Minus, -)
+	LIBRAPID_BINARY_OPERATION(Multiply, *)
+	LIBRAPID_BINARY_OPERATION(Divide, /)
 } // namespace librapid
 
 #endif // LIBRAPID_ARRAY_OPERATIONS_HPP
--- librapid/include/librapid/core/librapidPch.hpp
+++ librapid/include/librapid/core/librapidPch.hpp
@@ -35,6 +35,11 @@
 #include <random>
 #include <regex>
 #include <set>
+#include <utility>
+
+#if defined(LIBRAPID_HAS_OMP)
+#	include <omp.h>
+#endif // LIBRAPID_HAS_OMP
 
 #if defined(_WIN32) || defined(_WIN64)
 #	define WIN32_LEAN_AND_MEAN
