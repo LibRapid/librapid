ff14dacb590e81a69b4070fc917f9a12c4a2ea04
Toby Davis <pencilcaseman@gmail.com>
2022-11-03 10:33:48 +0000

    Cuda Storage
    Former-commit-id: 2518151d88a6d900bff660f968d03e973f951ff6

--- librapid/include/librapid/array/cudaStorage.hpp
+++ librapid/include/librapid/array/cudaStorage.hpp
@@ -1,6 +1,8 @@
 #ifndef LIBRAPID_ARRAY_CUDA_STORAGE_HPP
 #define LIBRAPID_ARRAY_CUDA_STORAGE_HPP
 
+#if defined(LIBRAPID_HAS_CUDA)
+
 /*
  * Implement a storage class for CUDA data structures. It will expose
  * the same functions as the `librapid::Storage` class, but the underlying
@@ -54,8 +56,99 @@ namespace librapid {
 		/// \tparam V Type of the elements in the initializer list
 		/// \param list Initializer list of elements
 		template<typename V>
-        explicit CudaStorage(const std::initializer_list<V> &list);
-	}
+		LIBRAPID_ALWAYS_INLINE CudaStorage(const std::initializer_list<V> &list);
+
+		/// Create a CudaStorage object from an std::vector of values
+		/// \tparam V Vector element type
+		/// \param vec The vector to fill with
+		template<typename V>
+		LIBRAPID_ALWAYS_INLINE explicit CudaStorage(const std::vector<V> &vec);
+
+		/// Assignment operator for a CudaStorage object
+		/// \param other CudaStorage object to copy
+		/// \return *this
+		LIBRAPID_ALWAYS_INLINE CudaStorage &operator=(const CudaStorage &other);
+
+		/// Move assignment operator for a CudaStorage object
+		/// \param other CudaStorage object to move
+		/// \return *this
+		LIBRAPID_ALWAYS_INLINE CudaStorage &operator=(CudaStorage &&other) noexcept;
+
+		/// Free a CudaStorage object
+		~CudaStorage();
+
+		/// Resize a CudaStorage object to \p size elements. Existing elements are preserved where
+		/// possible.
+		/// \param size Number of elements
+		/// \see resize(SizeType, int)
+		LIBRAPID_ALWAYS_INLINE void resize(SizeType newSize);
+
+		/// Resize a CudaStorage object to \p size elements. Existing elements are not preserved.
+		/// This method of resizing is faster and more efficient than the version which preserves
+		/// the original data, but of course, this has the drawback that data will be lost.
+		/// \param size Number of elements
+		LIBRAPID_ALWAYS_INLINE void resize(SizeType newSize, int);
+
+		/// Return the number of elements in the CudaStorage object.
+		/// \return The number of elements
+		LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE SizeType size() const noexcept;
+
+		/// Returns the pointer to the first element of the CudaStorage object
+		/// \return Pointer to the first element of the CudaStorage object
+		LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE Pointer data() const noexcept;
+
+	private:
+		// Copy data from \p begin to \p end into this Storage object
+		/// \tparam P Pointer type
+		/// \param begin Beginning of data to copy
+		/// \param end End of data to copy
+		template<typename P>
+		LIBRAPID_ALWAYS_INLINE void initData(P begin, P end);
+
+		/// Resize the Storage Object to \p newSize elements, retaining existing
+		/// data.
+		/// \param newSize New size of the Storage object
+		LIBRAPID_ALWAYS_INLINE void resizeImpl(SizeType newSize, int);
+
+		/// Resize the Storage object to \p newSize elements. Note this does not
+		/// initialize the new elements or maintain existing data.
+		/// \param newSize New size of the Storage object
+		LIBRAPID_ALWAYS_INLINE void resizeImpl(SizeType newSize);
+
+		Pointer m_begin = nullptr; // It is more efficient to store pointers to the start
+		Pointer m_end	= nullptr; // and end of the data block than to store the size
+	};
+
+	namespace detail {
+		/// Safely allocate memory for \p size elements of type on the GPU using CUDA.
+		/// \tparam T Scalar type
+		/// \param size Number of elements to allocate
+		/// \return GPU pointer
+		/// \see safeAllocate
+		template<typename T>
+		T *__restrict cudaSafeAllocate(size_t size) {
+			static_assert(typetraits::TriviallyDefaultConstructible<T>::value,
+						  "Data type must be trivially constructable for use with CUDA");
+			T *result;
+			cudaSafeCall(cudaMallocAsync(&result, sizeof(T) * size, global::cudaStream));
+			return result;
+		}
+
+		/// Safely free memory for \p size elements of type on the GPU using CUDA.
+		/// \tparam T Scalar type
+		/// \param data The data to deallocate
+		/// \return GPU pointer
+		/// \see safeAllocate
+		template<typename T>
+		void cudaSafeDeallocate(T *__restrict data) {
+			static_assert(typetraits::TriviallyDefaultConstructible<T>::value,
+						  "Data type must be trivially constructable for use with CUDA");
+			T *result;
+			cudaSafeCall(cudaFreeAsync(data, global::cudaStream));
+			return result;
+		}
+	} // namespace detail
 } // namespace librapid
 
+#endif // LIBRAPID_HAS_CUDA
 #endif // LIBRAPID_ARRAY_CUDA_STORAGE_HPP
--- librapid/src/global.cpp
+++ librapid/src/global.cpp
@@ -5,4 +5,8 @@ namespace librapid::global {
 	int64_t multithreadThreshold	 = 5000;
 	int64_t gemmMultithreadThreshold = 100;
 	int64_t numThreads				 = 1;
+
+#if defined(LIBRAPID_HAS_CUDA)
+	cudaStream_t cudaStream;
+#endif // LIBRAPID_HAS_CUDA
 } // namespace librapid::global
