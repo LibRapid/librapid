47b14beb7335ba43d18f3027cf2225d2845f9e36
Pencilcaseman <pencilcaseman@gmail.com>
2022-11-03 17:59:37 +0000

    Initial files
    Former-commit-id: be47646880b198f4c4441a6c101305279a623281 [formerly 5e4485157b6f84826e819c3b723a4b2df7f338d5]
    Former-commit-id: 19fd2d12c439ec7154cca9b95b67a77a4c4cee75

--- librapid/include/librapid/core/cudaConfig.hpp
+++ librapid/include/librapid/core/cudaConfig.hpp
@@ -5,10 +5,10 @@
 #ifdef LIBRAPID_HAS_CUDA
 
 // Under MSVC, supress a few warnings
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable : 4505) // unreferenced local function has been removed
-#endif
+#	ifdef _MSC_VER
+#		pragma warning(push)
+#		pragma warning(disable : 4505) // unreferenced local function has been removed
+#	endif
 
 #	define CUDA_NO_HALF // Ensure the cuda_helpers "half" data type is not defined
 #	include <cublas_v2.h>
@@ -16,9 +16,9 @@
 #	include <curand.h>
 #	include <curand_kernel.h>
 
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
+#	ifdef _MSC_VER
+#		pragma warning(pop)
+#	endif
 
 #	include "../vendor/jitify/jitify.hpp"
 
@@ -75,5 +75,7 @@ namespace librapid::device {
 	// Signifies that device memory should be used
 	struct GPU {};
 } // namespace librapid::device
+
+#include "../cuda/cudaHeaderLoader.hpp"
 
 #endif // LIBRAPID_CORE_CUDA_CONFIG_HPP
