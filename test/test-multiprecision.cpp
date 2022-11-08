4f171ed64ed348afd4b573b47a807a558f8eedf1
Toby Davis <pencilcaseman@gmail.com>
2022-11-08 21:17:47 +0000

    Fix tests
    Former-commit-id: bee4be2a4efc1d596c8b206b2e40d4a319eacbce [formerly e0617b96dc9f355a7f2b1c76918ed189dccc69b1]
    Former-commit-id: b741d0d435a3918b57dcbd5a887577360f30a6ef

--- test/test-multiprecision.cpp
+++ test/test-multiprecision.cpp
@@ -5,6 +5,8 @@
 
 namespace lrc = librapid;
 
+#if defined(LIBRAPID_USE_MULTIPREC)
+
 TEST_CASE("Test Multiprecision", "[multiprecision]") {
 	REQUIRE(lrc::mpz(1) == 1);
 	REQUIRE(lrc::mpq(1) == 1);
@@ -23,9 +25,9 @@ TEST_CASE("Test Multiprecision", "[multiprecision]") {
 
 	REQUIRE(fmt::format("{}", lrc::mpz(1234)) == "1234");
 	REQUIRE(fmt::format("{}", lrc::mpq(1234)) == "1234");
-	REQUIRE(fmt::format("{}", lrc::mpf(1234)) == "1234");
-	REQUIRE(fmt::format("{}", lrc::mpfr(1234)) == "1234");
-	REQUIRE(fmt::format("{}", lrc::mpfr(3.1415926)) == "3.1415926");
+	REQUIRE(fmt::format("{}", lrc::mpf(1234)) == "1234.0");
+	REQUIRE(fmt::format("{}", lrc::mpfr(1234)) == "1234.00000000000000");
+	REQUIRE(fmt::format("{}", lrc::mpfr(3.1415926)) == "3.14159260000000");
 
 	REQUIRE(lrc::mpz("1234") == 1234);
 	REQUIRE(lrc::mpq("1234") == 1234);
@@ -33,13 +35,56 @@ TEST_CASE("Test Multiprecision", "[multiprecision]") {
 	REQUIRE(lrc::mpf("1234") == 1234);
 	REQUIRE(lrc::mpfr("1234") == 1234);
 
+	REQUIRE(lrc::abs(lrc::sin(lrc::constPi()) - 0) < lrc::exp10(-15));
+	REQUIRE(lrc::abs(lrc::cos(lrc::constPi()) - -1) < lrc::exp10(-15));
+	REQUIRE(lrc::abs(lrc::tan(lrc::constPi()) - 0) < lrc::exp10(-15));
+
 	lrc::prec(500);
-	REQUIRE(lrc::constPi() ==
-			"3."
-			"14159265358979323846264338327950288419716939937510582097494459230781640628620899862803"
-			"48253421170679821480865132823066470938446095505822317253594081284811174502841027019385"
-			"21105559644622948954930381964428810975665933446128475648233786783165271201909145648566"
-			"92346034861045432664821339360726024914127372458700660631558817488152092096282925409171"
-			"53643678925903600113305305488204665213841469519415116094330572703657595919530921861173"
-			"819326117931051185480744623799627495673518857527248912279381830119491");
+	REQUIRE(
+	  lrc::constPi() ==
+	  "3."
+	  "14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534"
+	  "21170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446"
+	  "22948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326"
+	  "64821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053"
+	  "05488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799"
+	  "627495673518857527248912279381830119491304781324640566153215447811706812394506641467563306");
+
+	SECTION("Benchmarks") {
+		for (int64_t prec = 16; prec <= 66536; prec <<= 1) {
+			lrc::prec2(prec);
+
+			lrc::mpz bigInt(1);
+			bigInt <<= prec;
+			BENCHMARK(fmt::format("Integer Addition\n[{} bits]", prec)) {
+				return bigInt + bigInt;
+			};
+
+			BENCHMARK(fmt::format("Integer Multiplication\n[{} bits]", prec)) {
+				return bigInt * bigInt;
+			};
+
+			lrc::mpq bigRat = lrc::mpq(bigInt) / lrc::mpq(bigInt + 1);
+			BENCHMARK(fmt::format("Rational Addition\n[{} bits]", prec)) {
+				return bigRat + bigRat;
+			};
+
+			lrc::mpfr bigFloat = lrc::constPi();
+			BENCHMARK(fmt::format("Floating Point Addition\n[{} bits]", prec)) {
+				return bigFloat + bigFloat;
+			};
+
+			BENCHMARK(fmt::format("Floating Point Multiplication\n[{} bits]", prec)) {
+				return bigFloat * bigFloat;
+			};
+
+			BENCHMARK("Pi Calculation") { return lrc::constPi(); };
+		}
+	}
 }
+
+#else
+
+TEST_CASE("INVALID -- MultiPrecision not Enabled", "[multiprecision]") { REQUIRE(false); }
+
+#endif // LIBRAPID_USE_MULTIPRECISION
