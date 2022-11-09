8932d53546a2482bb54a0941f3cc0a59aba4ebc7
Toby <pencilcaseman@gmail.com>
2022-11-09 21:11:17 +0000

    Auto stash before merge of "master" and "origin/master"
    Former-commit-id: fcca28c298ec0e6b04b60f578555979b3a88cd1e

--- librapid/include/librapid/math/multiprec.hpp
+++ librapid/include/librapid/math/multiprec.hpp
@@ -136,34 +136,34 @@ namespace librapid {
 
 	// Trigonometric Functionality for mpf
 
-	/// Sine of a multiprecision floating point value: \f$ \sin(x) \f$
+	/// Sine of a multiprecision floating point value: \f$ \sin (x) \f$
 	/// \param val The value to take the sine of
 	/// \return The sine of the value
 	mpfr sin(const mpfr &val);
 
-	/// Cosine of a multiprecision floating point value: \f$ \cos(x) \f$
+	/// Cosine of a multiprecision floating point value: \f$ \cos (x) \f$
 	/// \param val The value to take the cosine of
 	/// \return The cosine of the value
 	mpfr cos(const mpfr &val);
 
-	/// Tangent of a multiprecision floating point value: \f$ \tan(x) \f$
+	/// Tangent of a multiprecision floating point value: \f$ \tan (x) \f$
 	/// \param val The value to take the tangent of
 	/// \return The tangent of the value
 	mpfr tan(const mpfr &val);
 
-	/// Arcsine of a multiprecision floating point value: \f$ \sin^{-1}(x) \f$
+	/// Arcsine of a multiprecision floating point value: \f$ \sin^{-1} (x) \f$
 	/// \param val The value to take the arcsine of
 	/// \return The arcsine of the value
 	/// \see sin(const mpfr &)
 	mpfr asin(const mpfr &val);
 
-	/// Arccosine of a multiprecision floating point value: \f$ \cos^{-1}(x) \f$
+	/// Arccosine of a multiprecision floating point value: \f$ \cos^{-1} (x) \f$
 	/// \param val The value to take the arccosine of
 	/// \return The arccosine of the value
 	/// \see cos(const mpfr &)
 	mpfr acos(const mpfr &val);
 
-	/// Arctangent of a multiprecision floating point value: \f$ \tan^{-1}(x) \f$
+	/// Arctangent of a multiprecision floating point value: \f$ \tan^{-1} (x) \f$
 	/// \param val The value to take the arctangent of
 	/// \return The arctangent of the value
 	/// \see tan(const mpfr &)
@@ -175,64 +175,64 @@ namespace librapid {
 	/// \return The atan2 of the value
 	mpfr atan2(const mpfr &dy, const mpfr &dx);
 
-	/// Cosec of a multiprecision floating point value: \f$ \csc(x) \f$
+	/// Cosec of a multiprecision floating point value: \f$ \csc (x) \f$
 	/// \param val The value to take the cosec of
 	/// \return The cosec of the value
 	mpfr csc(const mpfr &val);
 
-	/// Secant of a multiprecision floating point value: \f$ \sec(x) \f$
+	/// Secant of a multiprecision floating point value: \f$ \sec (x) \f$
 	/// \param val The value to take the secant of
 	/// \return The secant of the value
 	mpfr sec(const mpfr &val);
 
-	/// Cotangent of a multiprecision floating point value: \f$ \cot(x) \f$
+	/// Cotangent of a multiprecision floating point value: \f$ \cot (x) \f$
 	/// \param val The value to take the cotangent of
 	/// \return The cotangent of the value
 	mpfr cot(const mpfr &val);
 
-	/// Arccosec of a multiprecision floating point value: \f$ \csc^{-1}(x) \f$
+	/// Arccosec of a multiprecision floating point value: \f$ \csc^{-1} (x) \f$
 	/// \param val The value to take the arccosec of
 	/// \return The arccosec of the value
 	mpfr acsc(const mpfr &val);
 
-	/// Arcsecant of a multiprecision floating point value: \f$ \sec^{-1}(x) \f$
+	/// Arcsecant of a multiprecision floating point value: \f$ \sec^{-1} (x) \f$
 	/// \param val The value to take the arcsecant of
 	/// \return The arcsecant of the value
 	mpfr asec(const mpfr &val);
 
-	/// Arccotangent of a multiprecision floating point value: \f$ \cot^{-1}(x) \f$
+	/// Arccotangent of a multiprecision floating point value: \f$ \cot^{-1} (x) \f$
 	/// \param val The value to take the arccotangent of
 	/// \return The arccotangent of the value
 	mpfr acot(const mpfr &val);
 
 	// Hyperbolic Functionality for mpf
 
-	/// Hyperbolic sine of a multiprecision floating point value: \f$ \sinh(x) \f$
+	/// Hyperbolic sine of a multiprecision floating point value: \f$ \sinh (x) \f$
 	/// \param val The value to take the hyperbolic sine of
 	/// \return The hyperbolic sine of the value
 	mpfr sinh(const mpfr &val);
 
-	/// Hyperbolic cosine of a multiprecision floating point value: \f$ \cosh(x) \f$
+	/// Hyperbolic cosine of a multiprecision floating point value: \f$ \cosh (x) \f$
 	/// \param val The value to take the hyperbolic cosine of
 	/// \return The hyperbolic cosine of the value
 	mpfr cosh(const mpfr &val);
 
-	/// Hyperbolic tangent of a multiprecision floating point value: \f$ \tanh(x) \f$
+	/// Hyperbolic tangent of a multiprecision floating point value: \f$ \tanh (x) \f$
 	/// \param val The value to take the hyperbolic tangent of
 	/// \return The hyperbolic tangent of the value
 	mpfr tanh(const mpfr &val);
 
-	/// Hyperbolic arcsine of a multiprecision floating point value: \f$ \sinh^{-1}(x) \f$
+	/// Hyperbolic arcsine of a multiprecision floating point value: \f$ \sinh^{-1} (x) \f$
 	/// \param val The value to take the hyperbolic arcsine of
 	/// \return The hyperbolic arcsine of the value
 	mpfr asinh(const mpfr &val);
 
-	/// Hyperbolic arccosine of a multiprecision floating point value: \f$ \cosh^{-1}(x) \f$
+	/// Hyperbolic arccosine of a multiprecision floating point value: \f$ \cosh^{-1} (x) \f$
 	/// \param val The value to take the hyperbolic arccosine of
 	/// \return The hyperbolic arccosine of the value
 	mpfr acosh(const mpfr &val);
 
-	/// Hyperbolic arctangent of a multiprecision floating point value: \f$ \tanh^{-1}(x) \f$
+	/// Hyperbolic arctangent of a multiprecision floating point value: \f$ \tanh^{-1} (x) \f$
 	/// \param val The value to take the hyperbolic arctangent of
 	/// \return The hyperbolic arctangent of the value
 	mpfr atanh(const mpfr &val);
@@ -303,23 +303,23 @@ namespace librapid {
 	/// \return The ldexp of the value
 	mpfr ldexp(const mpfr &val, int exponent);
 
-	/// Logarithm of a multiprecision floating point value: \f$ \log(x) \f$
+	/// Logarithm of a multiprecision floating point value: \f$ \log (x) \f$
 	/// \param val The value to take the logarithm of
 	/// \return The logarithm of the value
 	mpfr log(const mpfr &val);
 
-	/// Logarithm of a multiprecision floating point value with a given base: \f$ \log_b(x) \f$
+	/// Logarithm of a multiprecision floating point value with a given base: \f$ \log_b (x) \f$
 	/// \param val The value to take the logarithm of
 	/// \param base The base to take the logarithm with
 	/// \return The logarithm of the value with the given base
 	mpfr log(const mpfr &val, const mpfr &base);
 
-	/// Logarithm of a multiprecision floating point value with base 2: \f$ \log_2(x) \f$
+	/// Logarithm of a multiprecision floating point value with base 2: \f$ \log_2 (x) \f$
 	/// \param val The value to take the logarithm of
 	/// \return The logarithm of the value with base 2
 	mpfr log2(const mpfr &val);
 
-	/// Logarithm of a multiprecision floating point value with base 10: \f$ \log_{10}(x) \f$
+	/// Logarithm of a multiprecision floating point value with base 10: \f$ \log_{10} (x) \f$
 	/// \param val The value to take the logarithm of
 	/// \return The logarithm of the value with base 10
 	mpfr log10(const mpfr &val);
