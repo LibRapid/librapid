050da22ae2b8ff6e121073094052b32c2dd85a12
Toby Davis <pencilcaseman@gmail.com>
2022-11-16 18:52:13 +0000

    Remove some warnings and (hopefully) fix some bugs

--- librapid/include/librapid/array/storage.hpp
+++ librapid/include/librapid/array/storage.hpp
@@ -416,7 +416,13 @@ namespace librapid {
 		auto size = static_cast<SizeType>(std::distance(begin, end));
 		m_begin	  = detail::safeAllocate(m_allocator, size);
 		m_end	  = m_begin + size;
-		std::uninitialized_copy(begin, end, m_begin);
+		if (typetraits::TriviallyDefaultConstructible<T>::value) {
+			// Use a slightly faster memcpy if the type is trivially default constructible
+			std::uninitialized_copy(begin, end, m_begin);
+		} else {
+			// Otherwise, use the standard copy algorithm
+			std::copy(begin, end, m_begin);
+		}
 	}
 
 	template<typename T, typename A>
