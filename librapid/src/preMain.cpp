80fa2e1d239ea520e0082f5276c8c6093b80fa63
Pencilcaseman <pencilcaseman@gmail.com>
2022-11-16 20:45:14 +0000

    Fix preMain

--- librapid/src/preMain.cpp
+++ librapid/src/preMain.cpp
@@ -1,13 +1,16 @@
 #include <librapid/librapid.hpp>
 
 namespace librapid::detail {
-	PreMain::PreMain() {
-#if defined(LIBRAPID_WINDOWS)
-		// Force the terminal to accept ANSI characters
-		system(("chcp " + std::to_string(CP_UTF8)).c_str());
-#endif // LIBRAPID_WINDOWS
-	}
+	bool preMainRun = false;
 
-	// Call the constructor of PreMain to run the pre-main code
-	PreMain preMain = PreMain();
+	PreMain::PreMain() {
+		if (!preMainRun) {
+#if defined(LIBRAPID_WINDOWS)
+			// Force the terminal to accept ANSI characters
+			system(("chcp " + std::to_string(CP_UTF8)).c_str());
+#endif // LIBRAPID_WINDOWS
+
+			preMainRun = true;
+		}
+	}
 } // namespace librapid::detail
