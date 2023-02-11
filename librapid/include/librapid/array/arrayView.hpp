ae12fc076578da09ba0020105cc6c18d0ab0a2ec
Toby <pencilcaseman@gmail.com>
2023-02-11 20:50:23 +0000

    Auto stash before merge of "develop" and "origin/develop"
    Former-commit-id: e2922570ca7b169806b9138dad8f4c850c72773b

--- librapid/include/librapid/array/arrayView.hpp
+++ librapid/include/librapid/array/arrayView.hpp
@@ -25,17 +25,24 @@ namespace librapid {
 			using ShapeType		 = typename BaseType::ShapeType;
 			using Device		 = typename typetraits::TypeInfo<BaseType>::Device;
 
+			/// Default constructor should never be used
 			ArrayView() = delete;
+
+			/// Copy an ArrayView object (not const)
+			/// \param array The array to copy
 			ArrayView(ArrayType &array);
+
+			/// Copy an ArrayView object (const)
+			/// \param other The array to copy
 			ArrayView(const ArrayView &other) = default;
 
-			/// Constructs an ArrayView from a temporary ArrayView.
-			/// \param other The ArrayView to move.
+			/// Constructs an ArrayView from a temporary instance
+			/// \param other The ArrayView to move
 			ArrayView(ArrayView &&other) = default;
 
-			/// Assigns an ArrayView to this ArrayView.
+			/// Assigns another ArrayView object to this ArrayView.
 			/// \param other The ArrayView to assign.
-			/// \return A reference to this ArrayView.
+			/// \return A reference to this
 			ArrayView &operator=(const ArrayView &other) = default;
 
 			/// Assigns a temporary ArrayView to this ArrayView.
@@ -43,6 +50,11 @@ namespace librapid {
 			/// \return A reference to this ArrayView.
 			ArrayView &operator=(ArrayView &&other) noexcept = default;
 
+			/// Assign a scalar value to this ArrayView. This function should only be used to
+			/// assign to a zero-dimensional "scalar" ArrayView, and will throw an error if used
+			/// incorrectly.
+			/// \param scalar The scalar value to assign
+			/// \return A reference to this
 			ArrayView &operator=(const Scalar &scalar);
 
 			/// Access a sub-array of this ArrayView.
@@ -58,23 +70,57 @@ namespace librapid {
 			template<typename CAST = Scalar>
 			LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE CAST get() const;
 
+			/// Same functionality as "get", except slightly less robust for user-defined types.
+			/// \tparam CAST Type to cast to
+			/// \return The scalar represented by the ArrayView object
 			template<typename CAST>
 			LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE operator CAST() const;
 
+			/// Access the underlying shape of this ArrayView
+			/// \return Shape object
 			LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE ShapeType shape() const;
+
+			/// Access the stride of this ArrayView
+			/// \return Stride object
 			LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE StrideType stride() const;
+
+			/// Access the offset of this ArrayView. This is the offset, in elements, from the
+			/// referenced Array's first element.
+			/// \return Offset
 			LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE int64_t offset() const;
 
+			/// Set the Shape of this ArrayView to something else. Intended for internal use only.
+			/// \param shape The new shape of this ArrayView
 			void setShape(const ShapeType &shape);
+
+			/// Set the Stride of this ArrayView to something else. Intended for internal use only.
+			/// \param stride The new stride of this ArrayView
 			void setStride(const StrideType &stride);
+
+			/// Set the offset of this ArrayView object. Intended for internal use only.
+			/// \param offset The new offset of this ArrayView
 			void setOffset(const int64_t &offset);
 
+			/// Returns the number of dimensions of this ArrayView
+			/// \return Number of dimensions
 			LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE int64_t ndim() const;
 
+			/// Return the Scalar at a given index in this ArrayView. This is intended for use
+			/// internally, but can be used externally too.
+			/// \param index The index of the Scalar to return
+			/// \return Scalar at the given index
 			LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE auto scalar(int64_t index) const;
 
+			/// Evaluate the contents of this ArrayView object and return an Array instance from
+			/// it. Depending on your use case, this may result in more performant code, but the new
+			/// Array will not reference the original data in the ArrayView.
+			/// \return A new Array instance
 			LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE ArrayType eval() const;
 
+			/// Cast an ArrayView to a std::string, aligning items down the columns. A format
+			/// string can also be specified, which will be used to format the items to strings
+			/// \param format The format string
+			/// \return A std::string representation of this ArrayView
 			LIBRAPID_NODISCARD std::string str(const std::string &format = "{}") const;
 
 		private:
@@ -115,7 +161,7 @@ namespace librapid {
 
 		template<typename T>
 		template<typename CAST>
-		CAST ArrayView<T>::get() const{
+		CAST ArrayView<T>::get() const {
 			LIBRAPID_ASSERT(m_shape.ndim() == 0,
 							"Can only cast a scalar ArrayView to a salar object");
 			return scalar(0);
