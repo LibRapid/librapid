33e563908d715e3774bf27017d419274c382537d
Toby Davis <pencilcaseman@gmail.com>
2023-02-24 09:25:48 +0000

    Update banner
    Former-commit-id: cb7565fa41f4f7aa235c89c3db61f38d10522092

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,9 @@ sys.path.insert(0, os.path.abspath("../.."))
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_dirs = [
-	"src/librapid/vendor",
-	"src/librapid/blas",
-	"src/librapid/cxxblas"
+    "src/librapid/vendor",
+    "src/librapid/blas",
+    "src/librapid/cxxblas"
 ]
 
 file_match = regex.compile(".*\..*")
@@ -31,16 +31,16 @@ currentMinorVersion = None
 currentPatchVersion = None
 
 try:
-	with open("../../version.txt") as versionFile:
-		text = versionFile.read()
-		currentMajorVersion = regex.search("MAJOR [0-9]+", text).group().split()[1]
-		currentMinorVersion = regex.search("MINOR [0-9]+", text).group().split()[1]
-		currentPatchVersion = regex.search("PATCH [0-9]+", text).group().split()[1]
-	print(f"Current Version: v{currentMajorVersion}.{currentMinorVersion}.{currentPatchVersion}")
+    with open("../../version.txt") as versionFile:
+        text = versionFile.read()
+        currentMajorVersion = regex.search("MAJOR [0-9]+", text).group().split()[1]
+        currentMinorVersion = regex.search("MINOR [0-9]+", text).group().split()[1]
+        currentPatchVersion = regex.search("PATCH [0-9]+", text).group().split()[1]
+    print(f"Current Version: v{currentMajorVersion}.{currentMinorVersion}.{currentPatchVersion}")
 except Exception as e:
-	print("[ ERROR ] Failed to read version.txt")
-	print(e)
-	sys.exit(1)
+    print("[ ERROR ] Failed to read version.txt")
+    print(e)
+    sys.exit(1)
 
 release = f"v{currentMajorVersion}.{currentMinorVersion}.{currentPatchVersion}"
 
@@ -50,13 +50,13 @@ master_doc = "index"
 # -- Run Doxygen -------------------------------------------------------------
 
 try:
-	# subprocess.run(["cd ../..", "doxygen html latex xml"])
-	os.system("cd ../.. && doxygen")
+    # subprocess.run(["cd ../..", "doxygen html latex xml"])
+    os.system("cd ../.. && doxygen")
 except Exception as e:
-	print("[ ERROR ] Failed to run doxygen")
-	print(e)
-	print("\n\nPlease run Doxygen yourself from the source directory")
-	sys.exit(1)
+    print("[ ERROR ] Failed to run doxygen")
+    print(e)
+    print("\n\nPlease run Doxygen yourself from the source directory")
+    sys.exit(1)
 
 # -- General configuration ---------------------------------------------------
 
@@ -64,27 +64,27 @@ except Exception as e:
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
-	# "furo", <= DO NOT comment back in -- it breaks everything
-	"sphinx.ext.autodoc",
-	"sphinx.ext.napoleon",
-	"sphinx.ext.coverage",
-	"sphinx.ext.autosectionlabel",
-	"sphinx.ext.intersphinx",
-	"sphinx.ext.mathjax",
-	"sphinx.ext.doctest",
-	"sphinx.ext.inheritance_diagram",
-	"sphinx_panels",
-	"breathe",
-	"exhale",
-	"numpydoc",
-	"sphinx-favicon"
+    # "furo", <= DO NOT comment back in -- it breaks everything
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.coverage",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.doctest",
+    "sphinx.ext.inheritance_diagram",
+    "sphinx_panels",
+    "breathe",
+    "exhale",
+    "numpydoc",
+    "sphinx-favicon"
 ]
 
 autosectionlabel_prefix_document = True
 
 # Set up the breathe extension
 breathe_projects = {
-	"librapid": "../xml"
+    "librapid": "../xml"
 }
 
 breathe_default_project = "librapid"
@@ -109,13 +109,13 @@ breathe_default_project = "librapid"
 
 # Set up the exhale extension
 exhale_args = {
-	# These arguments are required
-	"containmentFolder": "./api",
-	"rootFileName": "index.rst",
-	"rootFileTitle": "LibRapid",
-	"createTreeView": True,
-	"doxygenStripFromPath": "../..",
-	"verboseBuild": True
+    # These arguments are required
+    "containmentFolder": "./api",
+    "rootFileName": "index.rst",
+    "rootFileTitle": "LibRapid",
+    "createTreeView": True,
+    "doxygenStripFromPath": "../..",
+    "verboseBuild": True
 }
 
 # Add any paths that contain templates here, relative to this directory.
@@ -125,9 +125,9 @@ templates_path = ["_templates"]
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
-	"*jitify*",
-	"*fmt*",
-	"*blas/*"
+    "*jitify*",
+    "*fmt*",
+    "*blas/*"
 ]
 
 # -- Options for HTML output -------------------------------------------------
@@ -143,24 +143,30 @@ html_theme = "furo"
 html_logo = "../../branding/LibRapid small space.png"
 
 html_context = {
-	"github_user": "LibRapid",
-	"github_repo": "librapid",
-	"github_version": "master",
-	"doc_path": "docs/source",
+    "github_user": "LibRapid",
+    "github_repo": "librapid",
+    "github_version": "master",
+    "doc_path": "docs/source",
 }
 
 html_theme_options = {
-	# Show a banner at the top of the page
-	"announcement": """
-		 <a style=\"text-decoration: none; color: white;\" 
-			href=\"https://github.com/sponsors/Pencilcaseman\">
-			<img src=\"/en/latest/_static/LR_icon.png\"
-			width=22.5/> If you like LibRapid, please consider giving a small donation to help keep it alive!
-		 </a>
+    # Show a banner at the top of the page
+    "announcement": """
+<div style="white-space = word-break!important">
+    <img src="/en/latest/_static/LR_icon.png" alt="LibRapid" width="22.5"></img>
+    <a href="https://github.com/sponsors/Pencilcaseman">
+        <span class="banner-resize-text-lg">
+            If you like LibRapid, please consider giving a small donation to help keep it alive!
+        </span>
+        <span class="banner-resize-text-sm">
+            Please consider donating!
+        </span>
+    </a>
+</div>
 	""",
-	"sidebar_hide_name": True,
-	"light_logo": "LibRapid_light.png",
-	"dark_logo": "LibRapid_dark.png",
+    "sidebar_hide_name": True,
+    "light_logo": "LibRapid_light.png",
+    "dark_logo": "LibRapid_dark.png",
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -171,37 +177,37 @@ html_theme_options = {
 html_static_path = ["_static"]
 html_css_files = ['css/s4defs-roles.css']
 
-# html_favicon = "/en/latest/_static/LR_icon.png"
+# html_favicon = "/en/latest/_static/LR_icon_128.png"
 favicons = [
-	{
-		"rel": "icon",
-		"static-file": "LR_icon_128.png",
-		"type": "image/png",
-	},
-	{
-		"rel": "icon",
-		"sizes": "16x16",
-		"href": "LR_icon_128.png",
-		"type": "image/png",
-	},
-	{
-		"rel": "icon",
-		"sizes": "32x32",
-		"href": "LR_icon_128.png",
-		"type": "image/png",
-	},
-	{
-		"rel": "icon",
-		"sizes": "64x64",
-		"href": "LR_icon_128.png",
-		"type": "image/png",
-	},
-	{
-		"rel": "apple-touch-icon",
-		"sizes": "180x180",
-		"href": "LR_icon.png",
-		"type": "image/png",
-	},
+    {
+        "rel": "icon",
+        "static-file": "/en/latest/_static/LR_icon_128.png",
+        "type": "image/png",
+    },
+    {
+        "rel": "icon",
+        "sizes": "16x16",
+        "href": "/en/latest/_static/LR_icon_128.png",
+        "type": "image/png",
+    },
+    {
+        "rel": "icon",
+        "sizes": "32x32",
+        "href": "/en/latest/_static/LR_icon_128.png",
+        "type": "image/png",
+    },
+    {
+        "rel": "icon",
+        "sizes": "64x64",
+        "href": "/en/latest/_static/LR_icon_128.png",
+        "type": "image/png",
+    },
+    {
+        "rel": "apple-touch-icon",
+        "sizes": "180x180",
+        "href": "/en/latest/_static/LR_icon.png",
+        "type": "image/png",
+    },
 ]
 
 rst_prolog = """
