453c40a463da6cca651028fa29fc95099bc9e8f2
Toby Davis <pencilcaseman@gmail.com>
2023-03-20 12:48:33 +0000

    Multiprecision improvements

--- librapid/src/multiprecModAbs.cpp
+++ librapid/src/multiprecModAbs.cpp
@@ -4,6 +4,28 @@
 
 namespace librapid {
 	mpfr abs(const mpfr &val) { return ::mpfr::abs(val); }
+
+	mpf abs(const mpf &val) {
+		if (val >= 0)
+			return val;
+		else
+			return -val;
+	}
+
+	mpz abs(const mpz &val) {
+		if (val >= 0)
+			return val;
+		else
+			return -val;
+	}
+
+	mpq abs(const mpq &val) {
+		if (val >= 0)
+			return val;
+		else
+			return -val;
+	}
+
 	mpfr mod(const mpfr &val, const mpfr &mod) { return ::mpfr::fmod(val, mod); }
 } // namespace librapid
 
