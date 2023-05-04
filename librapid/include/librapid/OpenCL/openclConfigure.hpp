cb2df578708529f759298fd7133dd5e25de1c589
Toby <pencilcaseman@gmail.com>
2023-05-04 23:35:32 +0100

    Small fix

--- librapid/include/librapid/OpenCL/openclConfigure.hpp
+++ librapid/include/librapid/OpenCL/openclConfigure.hpp
@@ -2,6 +2,7 @@
 #define LIBRAPID_OPENCL_CONFIGURE_HPP
 
 namespace librapid {
+#if defined(LIBRAPID_HAS_OPENCL)
 	int64_t openclDeviceCompute(const cl::Device &device);
 	void updateOpenCLDevices(bool verbose = false);
 	cl::Device findFastestDevice(const std::vector<cl::Device> &devices);
@@ -9,6 +10,7 @@ namespace librapid {
 	void addOpenCLKernelSource(const std::string &source);
 	void addOpenCLKernelFile(const std::string &filename);
 	void compileOpenCLKernels();
+#endif // LIBRAPID_HAS_OPENCL
 } // namespace librapid
 
 #endif // LIBRAPID_OPENCL_CONFIGURE_HPP
