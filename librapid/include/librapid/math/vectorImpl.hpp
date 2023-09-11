831ad47ad0eb84f6f531fa8d15e30e2ca4f442f5
Pencilcaseman <pencilcaseman@gmail.com>
2023-09-11 20:09:54 +0100

    Update vector sum

--- librapid/include/librapid/math/vectorImpl.hpp
+++ librapid/include/librapid/math/vectorImpl.hpp
@@ -325,13 +325,15 @@ namespace librapid {
 			LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE auto sum() const -> Scalar {
 				Packet sum = Packet(0);
 				for (size_t i = 0; i < length; ++i) { sum += data[i]; }
-				return sum.sum();
+				// return sum.sum();
+				return xsimd::reduce_add(sum);
 			}
 
 			LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE auto sum2() const -> Scalar {
 				Packet sum = Packet(0);
 				for (size_t i = 0; i < length; ++i) { sum += data[i] * data[i]; }
-				return sum.sum();
+				// return sum.sum();
+				return xsimd::reduce_add(sum);
 			}
 
 			LIBRAPID_NODISCARD LIBRAPID_ALWAYS_INLINE const Packet &_get(size_t index) const {
